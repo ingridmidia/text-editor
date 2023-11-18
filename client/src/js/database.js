d5a476d8e0e510ae8eeffcc5ff63ef0d5ed7e700
ingridmidia <ingridmidia@gmail.com>
2023-11-18 17:38:30 -0600

    getDb function returns content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,6 +17,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
+  // Update the same record in the database https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/put
   const request = store.put({ content, id: 1 });
   const result = await request;
   console.log('Data saved to the database', result);
@@ -27,10 +28,12 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  // Always get the record stored with the id 1
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  // If result is undefined the function return undefined
+  return result?.content;
 };
 
 initdb();
