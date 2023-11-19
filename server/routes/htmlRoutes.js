7968fe1e69e1aad54b7066e043457de57705253e
ingridmidia <ingridmidia@gmail.com>
2023-11-19 15:32:22 -0600

    added route for favicon

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,14 @@
 const path = require('path');
 
-module.exports = (app) =>
+module.exports = (app) => {
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
   );
+
+  // Route for favicon
+  app.get('/favicon.ico', (req, res) =>
+    res.sendFile(path.join(__dirname, '../client/dist/favicon.ico'))
+  );
+
+  return app;
+}
